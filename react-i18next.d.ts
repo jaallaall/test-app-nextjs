b42f54f7f97b3f20ca1bf39de11f25d7063bfd4c
jaallaall <jalal.khaleqi22@gmail.com>
2022-06-18 12:41:42 +0430

    feat:edit translation

--- react-i18next.d.ts
+++ react-i18next.d.ts
@@ -1,9 +1,9 @@
-// import the original type declarations
-import "react-i18next";
-// import all namespaces (for the default language, only)
-import { Resources as MyResources } from "./types";
+import "next-i18next";
 
-declare module "react-i18next" {
-  // and extend them!
-  interface Resources extends MyResources {}
+import common from "./public/locales/en/common.json";
+
+export declare module "next-i18next" {
+  interface Resources {
+    common: typeof common;
+  }
 }
--- types.d.ts
+++
@@ -1,9 +0,0 @@
-import "next-i18next";
-
-import common from "./public/locales/en/common.json";
-
-export declare module "next-i18next" {
-  interface Resources {
-    common: typeof common;
-  }
-}
